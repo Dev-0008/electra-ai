4b4f9ba1cb6e37685ef109dbdc10e3f334b13bef
Dev-0008 <88734411+Dev-0008@users.noreply.github.com>
2026-05-13 22:49:46 +0530

    feat: added global app theme

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+
+    primaryColor: const Color(0xFF1E3A8A),
+
+    scaffoldBackgroundColor: const Color(0xFFF8FAFC),
+
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color(0xFF1E3A8A),
+      brightness: Brightness.light,
+    ),
+
+    textTheme: GoogleFonts.poppinsTextTheme(),
+
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      centerTitle: true,
+      iconTheme: const IconThemeData(
+        color: Colors.black,
+      ),
+      titleTextStyle: GoogleFonts.poppins(
+        color: Colors.black,
+        fontSize: 22,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+
+    cardTheme: CardTheme(
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      margin: const EdgeInsets.all(8),
+    ),
+  );
+}
